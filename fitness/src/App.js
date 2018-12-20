8a957a678bfefc959f1705d8c456d29e4c4c7de5
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-12-19 20:56:34 -0500

    Adding basic UI, can add and display exercises.

--- fitness/src/App.js
+++ fitness/src/App.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Paper } from '@material-ui/core'
+import { Paper, Typography, TextField, Button } from '@material-ui/core'
+import { List, ListItem, ListItemText } from '@material-ui/core'
 
 class App extends Component {
  state = {
-  
+  exercises: [],
+  title: ''
+ }
+ 
+ handleChange = (event) => {
+  this.setState({
+   [event.target.name]: event.target.value
+  })
+ }
+
+ handleAdd = (event) => {
+  event.preventDefault()
+  if (this.state.title) {
+  this.setState(({exercises, title}) => (
+   {
+    exercises: [...exercises,
+    {
+     title,
+     id: Date.now()
+    }
+    ],
+    title: ''
+   }
+  ))
+ }
  }
   render() {
+   const { title, exercises } = this.state
     return (
      <Paper>
-
+      <Typography
+       variant='display1'
+       align='center'
+       gutterBottom
+      >
+       Exercises
+      </Typography>
+      <form onSubmit={this.handleAdd}>
+      <TextField
+      name="title"
+      label="exercise"
+      value={title}
+      onChange={this.handleChange}
+      margin='normal'
+      />
+      <Button
+      type="submit"
+      color="primary"
+      variant="raised"
+      >
+       Add an excercise.
+      </Button>
+      <List>
+       {exercises.map(({id, title}) => <ListItem key={id}>
+        
+        <ListItemText primary={title}/>
+       </ListItem>)}
+      </List>
+      </form>
      </Paper>
     );
   }
