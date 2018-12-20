e445da144e1509e48d5a687082cd1da0640deaf3
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-12-19 20:18:36 -0500

    Adding react-app.

---
+++ fitness/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Paper } from '@material-ui/core'
+
+class App extends Component {
+ state = {
+  
+ }
+  render() {
+    return (
+     <Paper>
+
+     </Paper>
+    );
+  }
+}
+
+export default App;
